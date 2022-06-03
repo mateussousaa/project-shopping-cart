12263534af42baff9fe482845cf7c47c5624a9a2
mateussousaa <mateussousa20@outlook.com>
2022-06-03 15:12:13 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,5 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
   fail('Teste vazio');
 });
