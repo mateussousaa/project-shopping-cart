f80376669c8dabf1d18868e9d0a745a7b808db2d
mateussousaa <mateussousa20@outlook.com>
2022-06-03 17:25:58 -0300

    Requirement 03 - completed

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,23 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Should test if fetchItem is a function', async () => {
+    expect(typeof fetchItem).toBe('function');
+  });
+  it('Should test if the fetch was called', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toBeCalled();
+  });
+  it('Should test if the fetch was called with right endpoint', async () => {
+    await fetchItem('MLB1615760527');
+    const endpoint = 'https://api.mercadolibre.com/items/MLB1615760527';
+    expect(fetch).toBeCalledWith(endpoint);
+  });
+  it('Should test if the response is the expected response', async () => {
+    const response = await fetchItem('MLB1615760527');
+    expect(response).toEqual(item);
+  });
+  it('Should test if fetchItem is called with no parameter returns the expected error', async ()=> {
+    expect(fetchItem()).toEqual(new Error('You must provide an url'));
+  });
 });
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -5,13 +5,21 @@ const computadorSearch = require('../mocks/search');
 describe('1 - Teste a função fetchProducts', () => {
   it('Should test if fetchProducts is a function', async () => {
     expect(typeof fetchProducts).toBe('function');
-    // Test if the fetch was called with right endpoint
+  });
+  it('Should test if the fetch was called', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toBeCalled();
+  });
+  it('Should test if the fetch was called with right endpoint', async () => {
+    await fetchProducts('computador');
     const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
-    const response = await fetchProducts('computador');
     expect(fetch).toBeCalledWith(endpoint);
-    // Test if the response is the expected response
+  });
+  it('Should test if the response is the expected response', async () => {
+    const response = await fetchProducts('computador');
     expect(response).toEqual(computadorSearch);
-    // Test if fetchProducts is called with no parameter returns the expected error
+  });
+  it('Should test if fetchProducts is called with no parameter returns the expected error', async ()=> {
     expect(fetchProducts()).toEqual(new Error('You must provide an url'));
   });
 });
