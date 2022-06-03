94b8a6ace202385e0652612cb6abc9329672fd0a
mateussousaa <mateussousa20@outlook.com>
2022-06-03 15:46:09 -0300

    Requirement 01 - completed

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,5 +3,15 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  fail('Teste vazio');
+  it('Should test if fetchProducts is a function', async () => {
+    expect(typeof fetchProducts).toBe('function');
+    // Test if the fetch was called with right endpoint
+    const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    const response = await fetchProducts('computador');
+    expect(fetch).toBeCalledWith(endpoint);
+    // Test if the response is the expected response
+    expect(response).toEqual(computadorSearch);
+    // Test if fetchProducts is called with no parameter returns the expected error
+    expect(fetchProducts()).toEqual(new Error('You must provide an url'));
+  });
 });
